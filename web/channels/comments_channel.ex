b1dc266dc1d7b9cd85bd98643da929b4e0d1dbc5
evilosa <bojkov@sorc.ru>
2017-10-30 01:26:28 +0300

    Add base channel code.

--- web/channels/comments_channel.ex
+++ web/channels/comments_channel.ex
@@ -1,14 +1,16 @@
 defmodule Discuss.CommentsChannel do
   use Discuss.Web, :channel
 
-  def join(name, _params, socket) do
-    IO.puts("+++++++++")
-    IO.puts(name)
-    {:ok, %{hey: "there"}, socket}
+  alias Discuss.Topic
+
+  def join("comments:" <> topic_id, _params, socket) do
+    topic_id = String.to_integer(topic_id)
+    topic = Repo.get(Topic, topic_id)
+    
+    {:ok, %{}, socket}
   end
 
-  def handle_in(event, msg, socket) do
-    
-  end
-  
+  def handle_in(name, message, socket) do
+    {:reply, :ok, socket}
+  end  
 end
